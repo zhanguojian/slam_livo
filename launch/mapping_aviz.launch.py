e648d1177a556791af66173e75ca3713ce1e7775
guojian <sryg_zgj@163.com>
2026-05-16 10:18:39 +0800

    删除多余配置文件

--- launch/mapping_aviz.launch.py
+++ launch/mapping_aviz.launch.py
@@ -16,7 +16,7 @@ def generate_launch_description():
     rviz_config_file = os.path.join(get_package_share_directory("fast_livo"), "rviz_cfg", "fast_livo2.rviz")
 
     #Load parameters
-    avia_config_cmd = os.path.join(config_file_dir, "avia.yaml")
+    livox_config_cmd = os.path.join(config_file_dir, "livox.yaml")
     camera_config_cmd = os.path.join(config_file_dir, "camera_pinhole.yaml")
 
     # Param use_rviz
@@ -26,9 +26,9 @@ def generate_launch_description():
         description="Whether to launch Rviz2",
     )
 
-    avia_config_arg = DeclareLaunchArgument(
-        'avia_params_file',
-        default_value=avia_config_cmd,
+    livox_config_arg = DeclareLaunchArgument(
+        'livox_params_file',
+        default_value=livox_config_cmd,
         description='Full path to the ROS2 parameters file to use for fast_livo2 nodes',
     )
 
@@ -44,13 +44,13 @@ def generate_launch_description():
         default_value='True',
         description='Whether to respawn if a node crashes. Applied when composition is disabled.')
 
-    avia_params_file = LaunchConfiguration('avia_params_file')
+    livox_params_file = LaunchConfiguration('livox_params_file')
     camera_params_file = LaunchConfiguration('camera_params_file')
     use_respawn = LaunchConfiguration('use_respawn')
 
     return LaunchDescription([
         use_rviz_arg,
-        avia_config_arg,
+        livox_config_arg,
         camera_config_arg,
         use_respawn_arg,
 
@@ -96,7 +96,7 @@ def generate_launch_description():
             executable="fastlivo_mapping",
             name="laserMapping",
             parameters=[
-                avia_params_file,
+                livox_params_file,
             ],
             # https://docs.ros.org/en/humble/How-To-Guides/Getting-Backtraces-in-ROS-2.html
             prefix=[
